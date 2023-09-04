84679e4627f80bda912fe9b5973e93bf1ceee164
vamseeuxd <vamsee.uxd@gmail.com>
2023-09-04 13:32:40 +0530

    fix: dashboard layout issues fixed

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -10,14 +10,14 @@ export class DashboardPage {
   modules123: any[] = [];
 
   modules = [
-    { id: "Owners", profilePic: "/assets/img/owner.png", name: "Owners", title: "Owners" },
-    { id: "Accounts", profilePic: "/assets/img/accounts.png", name: "Accounts", title: "Accounts" },
-    { id: "Visitors", profilePic: "/assets/img/visitors.png", name: "Visitors", title: "Visitors" },
-    { id: "Notices", profilePic: "/assets/img/notices.png", name: "Notices", title: "Notices" },
-    { id: "Events", profilePic: "/assets/img/events.png", name: "Events", title: "Events" },
-    { id: "Rules", profilePic: "/assets/img/rules.png", name: "Rules", title: "Rules" },
-    { id: "Wings", profilePic: "/assets/img/wings.png", name: "Wings", title: "Wings" },
-    { id: "Flat", profilePic: "/assets/img/flat.png", name: "Flat", title: "Flat" },
+    { icon:"people-outline",  id: "Owners", profilePic: "/assets/img/owner.png", name: "Owners", title: "Owners" },
+    { icon:"newspaper-outline",  id: "Accounts", profilePic: "/assets/img/accounts.png", name: "Accounts", title: "Accounts" },
+    { icon:"people-circle-outline",  id: "Visitors", profilePic: "/assets/img/visitors.png", name: "Visitors", title: "Visitors" },
+    { icon:"alert-circle-outline",  id: "Notices", profilePic: "/assets/img/notices.png", name: "Notices", title: "Notices" },
+    { icon:"calendar-number-outline",  id: "Events", profilePic: "/assets/img/events.png", name: "Events", title: "Events" },
+    { icon:"checkmark-done-outline",  id: "Rules", profilePic: "/assets/img/rules.png", name: "Rules", title: "Rules" },
+    { icon:"business-outline",  id: "Wings", profilePic: "/assets/img/wings.png", name: "Wings", title: "Wings" },
+    { icon:"extension-puzzle-outline",  id: "Flat", profilePic: "/assets/img/flat.png", name: "Flat", title: "Flat" },
   ];
 
   constructor(public confData: ConferenceData) {}
