c29f80724e24b4e5d5d282d820e1b78d14fb930c
vamseeuxd <vamsee.uxd@gmail.com>
2023-09-04 19:56:20 +0530

    done dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -10,14 +10,15 @@ export class DashboardPage {
   modules123: any[] = [];
 
   modules = [
-    { icon:"people-outline",  id: "Owners", profilePic: "/assets/img/owner.png", name: "Owners", title: "Owners" },
-    { icon:"newspaper-outline",  id: "Accounts", profilePic: "/assets/img/accounts.png", name: "Accounts", title: "Accounts" },
-    { icon:"people-circle-outline",  id: "Visitors", profilePic: "/assets/img/visitors.png", name: "Visitors", title: "Visitors" },
-    { icon:"alert-circle-outline",  id: "Notices", profilePic: "/assets/img/notices.png", name: "Notices", title: "Notices" },
-    { icon:"calendar-number-outline",  id: "Events", profilePic: "/assets/img/events.png", name: "Events", title: "Events" },
-    { icon:"checkmark-done-outline",  id: "Rules", profilePic: "/assets/img/rules.png", name: "Rules", title: "Rules" },
-    { icon:"business-outline",  id: "Wings", profilePic: "/assets/img/wings.png", name: "Wings", title: "Wings" },
-    { icon:"extension-puzzle-outline",  id: "Flat", profilePic: "/assets/img/flat.png", name: "Flat", title: "Flat" },
+    { color:'primary', icon:"people-outline",  id: "Owners", profilePic: "/assets/img/owner.png", name: "Owners", title: "Owners" },
+    { color:'secondary', icon:"newspaper-outline",  id: "Accounts", profilePic: "/assets/img/accounts.png", name: "Accounts", title: "Accounts" },
+    { color:'tertiary', icon:"people-circle-outline",  id: "Visitors", profilePic: "/assets/img/visitors.png", name: "Visitors", title: "Visitors" },
+    { color:'success', icon:"alert-circle-outline",  id: "Notices", profilePic: "/assets/img/notices.png", name: "Notices", title: "Notices" },
+    { color:'warning', icon:"calendar-number-outline",  id: "Events", profilePic: "/assets/img/events.png", name: "Events", title: "Events" },
+    { color:'danger', icon:"checkmark-done-outline",  id: "Rules", profilePic: "/assets/img/rules.png", name: "Rules", title: "Rules" },
+    { color:'medium', icon:"business-outline",  id: "Apartments", profilePic: "/assets/img/wings.png", name: "Apartments", title: "Apartments" },
+    { color:'dark', icon:"grid-outline",  id: "Wings", profilePic: "/assets/img/wings.png", name: "Wings", title: "Wings" },
+    { color:'primary', icon:"scan-circle-outline",  id: "Flat", profilePic: "/assets/img/flat.png", name: "Flat", title: "Flat" },
   ];
 
   constructor(public confData: ConferenceData) {}
